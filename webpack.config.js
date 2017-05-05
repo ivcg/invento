ff5687d8bfecc11bd88ee1af5ab367a62af03aa7
Farhaan Bukhsh(fhackdroid) <farhaan.bukhsh@gmail.com>
2017-05-06 04:16:28 +0530

    Fix webpack config file to serve pages
    Signed-off-by: Farhaan Bukhsh(fhackdroid) <farhaan.bukhsh@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,68 @@
-var path = require('path'),
-  fs = require('fs'),
-  webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
+const fs = require('fs');
 
-var entry = {
-  'app': './app/index.jsx',
-  'invento': './sass/invento.sass'
-}
-
-var config = {
-  entry: entry,
-  context: path.join(__dirname, 'static/'),
+module.exports = {
+  entry: './static/app/index.jsx',
+  output: {
+    filename: 'build/bundle.js'
+  },
+  // To have a fallback mechanism for ``React Routes`` so that if it hits a
+  // 404 webpack-dev-server knows which file to serve
+  devServer: {
+    historyApiFallback: {
+      index: 'html/index.html'
+    }
+  },
   module: {
-    rules: [{
-      test: /\.sass$/,
-      use: [{
-        loader: "style-loader",
-        loader: "css-loader",
-        loader: "sass-loader",
-        options: {
-          includePaths: [
-            path.join(__dirname, 'node_modules/bootstrap/scss/'),
-          ]
-        }
-      }]
-    }, {
-      test: /\.jsx$/,
-      use: [{
-        loader: "babel-loader",
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
         query: {
           presets: ['es2015', 'react']
         }
-      }]
-    }]
+      }, {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          }, {
+            loader: "css-loader"
+          }
+        ]
+      }, {
+        test: /\.sass$/,
+        use: [
+          {
+            loader: "style-loader"
+          }, {
+            loader: "css-loader"
+          }, {
+            loader: "sass-loader",
+            options: {
+              strictMath: true,
+              noIeCompat: true
+            }
+          }
+        ]
+      }, {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: {
+          loader: 'file-loader?name=../fonts/[name].[ext]'
+        }
+      }
+    ]
   },
-  output: {
-    filename: '[name].bundle.js',
-    path: __dirname + '/build'
+  resolve: {
+    extensions: [
+      '.js', '.jsx'
+    ],
+    modules: [
+      path.resolve(__dirname, './static/app'),
+      'node_modules'
+    ]
   }
-}
+};
 
-module.exports = config;

458778376b0b761fa1b78f5fd0d544a3501787d1
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2017-05-06 16:11:42 +0530

    static: Setup redux, redux-thunk, and react file structure
    Signed-off-by: Sayan Chowdhury <sayan.chowdhury2012@gmail.com>

---
+++ static/app/containers/App.jsx
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import { connect } from 'react-redux';
+
+class App extends Component {
+  render() {
+  }
+}
+
+function mapStateToProps(state) {
+}
+
+export default connect(mapStateToProps)(App);
--- static/app/index.jsx
+++ static/app/index.jsx
@@ -1,7 +1,17 @@
+import 'babel-polyfill';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import App from './containers/App';
+import configureStore from './store/configureStore';
+
+const store = configureStore();
 
 ReactDOM.render(
-  <h1>Hello, World</h1>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );

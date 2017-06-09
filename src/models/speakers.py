37d183313cc5312ae6eb266591d68d7c4306a7ee
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2017-06-09 21:18:23 +0530

    models: Change the profile for Speaker to O2OField
    Signed-off-by: Sayan Chowdhury <sayan.chowdhury2012@gmail.com>

--- src/models/speakers.py
+++ src/models/speakers.py
@@ -4,7 +4,7 @@ from users.models import UserProfile
 
 
 class Speaker(models.Model):
-    profile = models.ForeignKey(UserProfile, unique=True, db_index=True)
+    profile = models.OneToOneField(UserProfile, db_index=True)
     about = models.TextField(max_length=500, blank=True, null=True)
     avatar = models.URLField(max_length=200, unique=True, blank=True,
                              null=True)

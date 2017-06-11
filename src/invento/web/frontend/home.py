e88e031d504cb6185c28a9b546b9f6454d4a5403
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2017-06-11 19:01:17 +0530

    web, templates: Create a template directory
    Signed-off-by: Sayan Chowdhury <sayan.chowdhury2012@gmail.com>

--- src/invento/web/frontend/home.py
+++ src/invento/web/frontend/home.py
@@ -1,7 +1,12 @@
-from django.http import HttpResponse
-from django.views import View
+from django.views.generic.base import TemplateView
 
+import random
+import string
 
-class HomeView(View):
-    def get(self, request):
-        return HttpResponse('Hello World')
+class HomePageView(TemplateView):
+    template_name = 'invento/homepage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        context['name'] = ''.join(random.sample(string.ascii_lowercase, 6))
+        return context
--- src/invento/web/urls.py
+++ src/invento/web/urls.py
@@ -3,9 +3,9 @@ from __future__ import absolute_import
 from django.conf.urls import url
 
 # internal imports
-from invento.web.frontend.home import HomeView
+from invento.web.frontend.home import HomePageView
 
 urlpatterns = [
-    url(r'^$', HomeView.as_view(),
+    url(r'^$', HomePageView.as_view(),
         name='invento'),
 ]
